44f584b33584c7798ce1e26db0b6d11ea24d6b60
“mayura” <mayurakharche@gmail.com>
2019-08-30 13:16:45 +0530

    [Mayura,Deepa]Refractor. Test and Class

--- src/main/com/tw/vapasi/Measurement.java
+++ src/main/com/tw/vapasi/Measurement.java
@@ -1,4 +1,49 @@
 package com.tw.vapasi;
 
-public class Measurement {
+import java.util.Objects;
+
+//Understand units of distance
+class Measurement {
+    private static final String METER = "M";
+    private static final String CENTIMETER = "CM";
+    private static final String KILOMETER = "KM";
+
+    private double value;
+    private String unit;
+
+    Measurement(double value, String unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    Measurement toCentiMeter() {
+        switch (this.unit) {
+            case METER:
+                return new Measurement(this.value * 100, CENTIMETER);
+            case KILOMETER:
+                return new Measurement(this.value * 100000, CENTIMETER);
+        }
+        return new Measurement(this.value / 100, METER);
+    }
+
+    @Override
+    public boolean equals(Object otherMeasurement) {
+        if (this == otherMeasurement) return true;
+
+        if ((otherMeasurement == null) || (otherMeasurement.getClass() != this.getClass()))
+            return false;
+
+        Measurement anotherMeasurement = (Measurement) otherMeasurement;
+        if (this.unit.compareTo(anotherMeasurement.unit) != 0)
+            return false;
+
+        return this.value == anotherMeasurement.value;
+
+    }
+
+
+    public int hashCode() {
+        return Objects.hash(this.value, this.unit);
+    }
+
 }
--- src/test/com/tw/vapasi/MeasurementTest.java
+++ src/test/com/tw/vapasi/MeasurementTest.java
@@ -1,4 +1,79 @@
 package com.tw.vapasi;
 
-public class MeasurementTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SuppressWarnings("ALL")
+class MeasurementTest {
+
+    @Test
+    void expectSameReferenceEqualsTrue() {
+        Measurement kilometer1 = new Measurement(1, "KM");
+
+        assertTrue(kilometer1.equals(kilometer1));
+    }
+
+    @Test
+    void expectNullReferenceEqualsFalse() {
+        Measurement kilometer1 = new Measurement(1, "KM");
+
+        assertFalse(kilometer1.equals(null));
+    }
+
+    @Test
+    void expectDifferentReferenceEqualsFalse() {
+        Measurement kilometer1 = new Measurement(1, "KM");
+
+        assertFalse(kilometer1.equals("tests"));
+    }
+
+    @Test
+    void expectObjectsWithSameValueEqualsTrue() {
+        Measurement kilometer = new Measurement(1, "KM");
+        Measurement anotherKilometer = new Measurement(1, "KM");
+
+        assertTrue(kilometer.equals(anotherKilometer));
+    }
+
+    @Test
+    void expect100CMEquals1M() {
+        Measurement centimeter100 = new Measurement(100, "CM");
+        Measurement meter1 = new Measurement(1, "M");
+
+        Measurement actual = meter1.toCentiMeter();
+
+        assertTrue(centimeter100.equals(actual));
+    }
+
+    @Test
+    void expect1KMEquals1000M() {
+        Measurement kilometer1 = new Measurement(1, "KM");
+        Measurement meter1000 = new Measurement(1000, "M");
+
+        Measurement meterToCentimeter = meter1000.toCentiMeter();
+        Measurement kilometerToCentimeter = kilometer1.toCentiMeter();
+
+        assertTrue(meterToCentimeter.equals(kilometerToCentimeter));
+    }
+
+    @Test
+    void expect1KMEquals100000CM() {
+        Measurement kilometer1 = new Measurement(1, "KM");
+        Measurement centimeter100000 = new Measurement(100000, "CM");
+
+        Measurement kilometerToCentimeter = kilometer1.toCentiMeter();
+
+        assertTrue(centimeter100000.equals(kilometerToCentimeter));
+    }
+
+    @Test
+    void expect1KMNotEquals1000CM() {
+        Measurement kilometer1 = new Measurement(1, "KM");
+        Measurement centimeter1000 = new Measurement(1000, "CM");
+
+        Measurement kilometerToCentimeter = kilometer1.toCentiMeter();
+
+        assertFalse(centimeter1000.equals(kilometerToCentimeter));
+    }
 }
